646b984450b850bff37d139a5505b7c067df7d61
DiegoLudena <d_l_m88@yahoo.es>
2023-02-23 12:05:34 +0100

    ejercicio uno funciona

--- src/UNO/Uno.java
+++ src/UNO/Uno.java
@@ -1,10 +1,25 @@
 package UNO;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Uno {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		
+        File ejerciciossrteams = new File("EjerciciosStreams");
+        if (ejerciciossrteams.exists()) {
+        	
+        }else {
+        	ejerciciossrteams.mkdir();
+        }
+        try {
+			FileWriter f1=new FileWriter("EjerciciosStreams/uno.txt");
+			FileWriter f2=new FileWriter("EjerciciosStreams/dos.txt");
+			FileWriter f3=new FileWriter("EjerciciosStreams/tres.txt");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+}
 }
